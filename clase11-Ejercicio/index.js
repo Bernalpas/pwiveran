c6ade33f9a8051d5e4434dace8714dd4c1e2762a
Berni <bernalpas@gmail.com>
2023-03-06 21:55:54 -0300

    Agregamos cambios del JS

--- clase11-Ejercicio/index.js
+++ clase11-Ejercicio/index.js
@@ -5,3 +5,22 @@ function prueba() {
 }
 
 prueba();
+
+const date = new Date();
+const hora = date.getHours();
+const minutos = date.getMinutes();
+const segundos = date.getSeconds();
+
+/* Creamos las funciones extras que necesitamos para el componente*/
+function damehora() {
+    console.log(hora);
+    console.log(minutos);
+    console.log(segundos);
+    return `${hora}:${minutos}:${segundos}`
+}
+
+    
+setTimeout(() => {
+    console.log(`${hora}:${minutos}:${segundos}`);
+}, 1000);
+
